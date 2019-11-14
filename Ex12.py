6cac049c938eed433e742b05d8f7a8553e3def8b
Bimal Singh <bimalsingh983@gmail.com>
2019-11-14 12:26:58 +0530

    Update Ex12.py

--- Ex12.py
+++ Ex12.py
@@ -1,10 +1,14 @@
-print "Ente the file to open"
+#Fetching the file name from the user 
+print "Enter the file to open"
 file1 = raw_input()
 
+#opening the file in the variable txt
 txt = open(file1)
 print "here's your file" 
+#printing the data of the file  
 print txt.read()
 
+#Performing similar command
 print "Enter the file name again"
 filename = raw_input()
 
