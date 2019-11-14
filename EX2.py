bde4aef4e2d18ce3b226d36239b2d3104aaf0e65
Bimal Singh <bimalsingh983@gmail.com>
2019-11-14 12:19:36 +0530

    Update EX2.py

--- EX2.py
+++ EX2.py
@@ -1,3 +1,4 @@
+#defining the variables and their values 
 my_name = 'Bimal Singh'
 my_age = 19
 my_height = 153
@@ -6,7 +7,8 @@ my_eyes = 'black'
 my_teeth = 'White'
 my_hair = 'Black'
 
-
+#using %var to print the value of variable
+#For int->%d , string->%s 
 print "Let's talk about %s" %my_name 
 print "He is %d centimeters tall" %my_height
 print "If I add %d, %d, and %d I get %d." % ( my_age, my_height, my_weight, my_age + my_height + my_weight)
