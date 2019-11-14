16be912d6ce8a110f8851b82fe1d25a0d20405d7
Bimal Singh <bimalsingh983@gmail.com>
2019-11-14 12:24:04 +0530

    Update Ex10.py

--- Ex10.py
+++ Ex10.py
@@ -1,7 +1,11 @@
+#importing 
 from sys import argv
 
+#For this the starting command in your terminal should be : python Ex10.py fir sec thr 
+#above fir sec and thr can be any values
 script, first, second, third = argv
 
+#printing the values from given values 
 print "The script is called:", script
 print "Your first variable is:", first
 print "Your secong variable is:", second
